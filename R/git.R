3e3afd3c446bae2bb45acf97faf36a6929c06eee
Stu Field <stu.g.field@gmail.com>
2022-03-06 11:34:03 -0700

    Add new `git_tag_info()`
    - new function to generate a data frame
      of the current tag information for  the repo

--- R/git.R
+++ R/git.R
@@ -327,3 +327,25 @@ git_sitrep <- function() {
     invisible()
   }
 }
+
+#' @describeIn git Gets a data frame summary of the current
+#' git repository tags.
+#' @export
+git_tag_info <- function() {
+  is_git()
+  no_tags <- identical(git("tag", "-l", echo_cmd = FALSE)$stdout, "")
+  if ( no_tags ) {
+    stop("No tags in repository ...", call. = FALSE)
+  }
+  tags <- git("tag", "--sort=-v:refname",
+    "--format='%(tag)\t%(objectname:short)\t%(object)\t%(subject)\t%(taggername)\t%(taggeremail)\t%(taggeremail:localpart)\t%(taggerdate)\t%(objectsize)'",
+    echo_cmd = FALSE)$stdout
+  tags <- strsplit(tags, "\t")
+  ret  <- data.frame(do.call(rbind, tags))
+  names(ret) <- c("tag", "tag_sha", "target_sha", "message",
+                  "author", "email", "user", "tagdate", "size")
+  rownames(ret)  <- ret$tag
+  ret$target_sha <- substr(ret$target_sha, 1, 7)
+  ret$path       <- file.path(normalizePath("."), ".git")
+  ret
+}
