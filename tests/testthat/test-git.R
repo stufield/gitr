9051b63c0652a78391cab4aece394c35ce70e158
Stu Field <stu.g.field@gmail.com>
2023-02-07 15:29:20 -0700

    Flow control unit test

--- tests/testthat/test-git.R
+++ tests/testthat/test-git.R
@@ -1,6 +1,8 @@
 
 test_that("the basic functionality of git() works as expected", {
+  skip_if(isTRUE(Sys.which("git") == ""))
+  skip_if_not(is_git())
   expect_invisible(out <- git("status", echo_cmd = FALSE))
-  expect_equal(out$status, 0)
+  expect_equal(out$status, 0L)
   expect_equal(out$stderr, "")
 })
