e9307d92a4cef7dbc7b5bc2beab8a015462cba2b
Stu Field <stu.g.field@gmail.com>
2023-02-10 11:39:55 -0700

    Update git_sitrep()

--- R/git-sitrep.R
+++ R/git-sitrep.R
@@ -8,30 +8,45 @@
 git_sitrep <- function() {
   if ( is_git() ) {
     cat("Using Git version:", slug_color(git_version(), "\033[34m"), "\n")
-    cat("\nCurrent Branch:", slug_color(git_current_br(),"\033[32m"), "\n")
-    cat("\nDefault Branch:", slug_color(git_default_br(),"\033[36m"), "\n")
-
-    cat("\nBranches:\n")
-    gba()
+    cat("\nCurrent branch:", slug_color(git_current_br(),"\033[32m"))
+    cat("\nDefault branch:", slug_color(git_default_br(),"\033[36m"), "\n")
 
     cat("\nRepo status:\n")
     gss()
 
-    br <- trimws(gsub("\\*", "", git("branch", echo_cmd = FALSE)$stdout))
-    rt <- trimws(git("branch", "-r", echo_cmd = FALSE)$stdout)
-    lgl <- vapply(br, function(.x) any(grepl(paste0(.x, "$"), rt)), NA)
-    br <- br[lgl]
-    up <- lapply(br, function(branch) {
-      be <- git("rev-list", "--count", paste0(branch, "..@{upstream}"),
-                echo_cmd = FALSE)$stdout
-      ah <- git("rev-list", "--count", paste0("@{upstream}..", branch),
-                echo_cmd = FALSE)$stdout
-      data.frame(branch = branch, ahead = ah, behind = be)
-    })
-    cat("\nUpstream remote:\n")
-    print(do.call(rbind, up))
+    cat("\nBranches:\n")
+    gba()
 
-    cat("\nCommit", slug_color(git_current_br(), "\033[32m"), "Log:\n")
+    br <- git_current_br()
+    be <- git("rev-list", "--count", paste0(br, "..@{upstream}"),
+              echo_cmd = FALSE)$stdout
+    ah <- git("rev-list", "--count", paste0("@{upstream}..", br),
+              echo_cmd = FALSE)$stdout
+    remote <- git("remote", "show", echo_cmd = FALSE)$stdout
+
+    cat("\nLocal status:\n")
+    if ( ah == 0 && be == 0 ) {
+      done("OK")
+    }
+    if ( ah > 0 ) {
+      cat("Your local branch", slug_color(br),
+          "is ahead of", slug_color(paste0(remote, "/", br), "\033[34m"),
+          "by", ah, "commits.\n")
+    }
+    if ( be > 0 ) {
+      cat("Your local branch", slug_color(br),
+          "is behind", slug_color(paste0(remote, "/", br), "\033[34m"),
+          "by", be, "commits.\n")
+    }
+
+    cat("\nUpstream remotes:", slug_color(remote, "\033[33m"), "\n")
+    br_verb <- git("branch", "-vv", echo_cmd = FALSE)$stdout
+    br_verb |>
+      gsub(pattern = "(^\\* [[:alnum:]|_-]+ )", replacement = "\033[32m\\1\033[0m") |>
+      gsub(pattern = "(origin/[[:alnum:]|_-]+)", replacement = "\033[34m\\1\033[0m") |>
+      cat(sep = "\n")
+
+    cat("\nCommit log:", slug_color(br, "\033[32m"), "\n")
     glog(5)
   } else {
     invisible(NULL)
