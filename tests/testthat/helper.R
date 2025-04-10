78b3c853e98dbe6e7b83012c416253936cf20aab
Stu Field <stu.g.field@gmail.com>
2025-04-10 09:43:20 -0600

    Set up special gitr unit testing fixtures
    - see: https://testthat.r-lib.org/articles/test-fixtures.html
    - special worktree for unit testing
    - cleans up when finished each test-*.R file
    - does the following:
      - sets `gitr_echo_cmd = FALSE`
      - creates git worktree for testing
      - sets wd to that worktree
      - returns to testing wd when complete
      - deletes git worktree when complete

---
+++ tests/testthat/helper.R
@@ -0,0 +1,35 @@
+
+local_create_worktree <- function(dir    = tempfile("gitr-"),
+                                  branch = "gitr-test-br",
+                                  env    = parent.frame()) {
+  withr::local_options(
+    list(gitr_echo_cmd = FALSE),
+    .local_envir = env
+  )
+
+  # get current directory
+  pwd <- getwd()
+
+  # create new directory
+  dir.create(dir)
+
+  # initialize testing git worktree repo
+  git("worktree", "add", "--track", "-B", branch, dir)
+
+  # rm local branch (last)
+  # local branch created automatically as worktree added
+  # remove local branch connected to worktree
+  withr::defer(git("branch", "-D", branch), envir = env)
+
+  # delete git worktree and delete directory when done
+  withr::defer(git("worktree", "remove", "--force", dir), envir = env)
+
+  # change working directory to worktree
+  setwd(dir)
+
+  # return to original pwd when done
+  withr::defer(setwd(pwd), envir = env)
+
+  invisible(dir)
+}
+
