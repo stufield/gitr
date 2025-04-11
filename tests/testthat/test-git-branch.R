9bbf7a244f7596d5e7e15be5b99b13e6afefc409
Stu Field <stu.g.field@gmail.com>
2025-04-10 19:11:48 -0600

    Cleaned up testing branches left over from worktree
    - the 'wip-foo' branch created in the worktree
      was supposed to be ephemeral but was
      being left behind after worktree removal
    - solution is to manually delete the test-branch
      DURING the unit tests, this also deletes
      the branch locally; then clean up as usual

--- tests/testthat/test-git-branch.R
+++ tests/testthat/test-git-branch.R
@@ -12,6 +12,10 @@ test_that("`git_default_br()` returns proper default", {
 test_that("`git_current_br()` returns proper default", {
   expect_equal(git_current_br(), "gitr-test-br")  # default
   new_br <- "wip-foo"
+  withr::defer({
+    git("checkout", "gitr-test-br")  # when finished; jump back to orig br
+    git("branch", "-D", new_br)      # and then delete the 'new_br' branch
+  })
   git_checkout(new_br)
   expect_equal(git_current_br(), new_br)
 })
