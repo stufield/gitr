e00125b75fcb4be006b902ad16eb47dde399b240
Stu Field <stu.g.field@gmail.com>
2025-04-11 09:54:59 -0600

    Cleaned up 'params' docs

--- R/params.R
+++ R/params.R
@@ -2,16 +2,16 @@
 #'
 #' @name params
 #'
-#' @param n `integer(1)`How far back to go from current `HEAD`.
+#' @param n `integer(1)`. How far back to go from current `HEAD`.
 #'   Same as the command line `git log -n` parameter.
 #'   For `git stash` commands, zero-index into the stash list.
 #'
-#' @param file `character(1)`. The path name to a file.
+#' @param file `character(1)`. The path to a file.
 #'
 #' @param branch `character(1)`. The name of a branch, typically a
 #'   feature branch.
 #'
-#' @param sha `character(1)`. The commit SHA-1 hash to pull messages from.
-#'   If `NULL`, the most recent commit on the current branch.
+#' @param sha `character(1)`. The commit secure hash algorithm (SHA-1).
+#'   If `NULL`, typicall reverts to the most recent commit on the current branch.
 #'
 NULL
